9f8de9eb6a3a00730d65005a4fc7236e365ab846
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-03 23:20:41 +0600

    Avater And Text using Faker Api

--- components/src/index.js
+++ components/src/index.js
@@ -6,14 +6,55 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
-      <CommentDetail author={faker.name.firstName()}></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        text={faker.lorem.words()}
+
+        // timeAgo={faker.date.past()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        // timeAgo={faker.date.past()}
+        text={faker.lorem.words()}
+      ></CommentDetail>
     </div>
   );
 };
