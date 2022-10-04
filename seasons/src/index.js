74ad47e8c7a6f7c0de49a640539189c7a2093919
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-05 00:12:45 +0600

    conditionally Rendering Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,13 +17,13 @@ class App extends React.Component {
     );
   }
   render() {
-    return (
-      <div>
-        <div>Latitude: {this.state.lat}</div>
-        <br />
-        <div>Error: {this.state.errorMessage}</div>
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Error: {this.state.lat}</div>;
+    }
+    return <div>Loading!!</div>;
   }
 }
 
