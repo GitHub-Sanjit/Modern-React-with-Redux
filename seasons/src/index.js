2a5ffd0613eee0d2f297c29e504f0c418eda697c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-05 00:01:32 +0600

    Handling Error Message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,17 +5,25 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        <div>Latitude: {this.state.lat}</div>
+        <br />
+        <div>Error: {this.state.errorMessage}</div>
+      </div>
+    );
   }
 }
 
