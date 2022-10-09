5a68619df49f9c102736d370be59b6112ad3e43a
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-09 23:31:46 +0600

    Searching for Videos

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBTFC3QtDWipyM4OU96UDcW3AlCGnrVcm0";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    naxResult: 5,
+    key: KEY,
+  },
+});
