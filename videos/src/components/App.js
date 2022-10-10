831f46030032104860337f8acff4948d5ad0d195
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-10 12:12:11 +0600

    Passing State as Props

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = { videos: [] };
@@ -18,8 +19,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>I Have{" "}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>
+        <VideoList videos={this.state.videos}></VideoList>
       </div>
     );
   }
---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoList = (props) => {
+  return (
+    <div>
+      <h1>{props.videos.length}</h1>
+    </div>
+  );
+};
+
+export default VideoList;
