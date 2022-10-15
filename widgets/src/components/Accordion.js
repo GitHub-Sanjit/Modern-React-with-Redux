af01c7c01504b2baf04619a820b07804acfa042f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-15 22:51:02 +0600

    Introducing useState

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items, index }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const onTitleClick = (index) => {
-    console.log("Title clicked", index);
+    setActiveIndex(index);
   };
 
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div
-          onClick={() => onTitleClick(index)}
-          className="title active"
-        >
+        <div onClick={() => onTitleClick(index)} className="title active">
           <i className="dropdown icon"></i>
           {item.title}
         </div>
@@ -22,7 +21,12 @@ const Accordion = ({ items, index }) => {
     );
   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
