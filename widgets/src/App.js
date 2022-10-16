dca0baf133263dfa5096b1435f6cc4d258d4f1c0
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-16 10:50:59 +0600

    Scaffolding the Dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
 
 const items = [
@@ -17,11 +18,27 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
   return (
     <div>
       {/* <Accordion items={items}></Accordion> */}
-      <Search></Search>
+      {/* <Search></Search> */}
+      <Dropdown options={options}></Dropdown>
     </div>
   );
 };
---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dropdown = () => {
+  return (
+    <div>
+      <h1>DropDown page</h1>
+    </div>
+  );
+};
+
+export default Dropdown;
