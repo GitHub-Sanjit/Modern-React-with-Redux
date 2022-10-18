4d8aad8df322554d52a30afd0c99e5dd8a6350c9
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-18 20:35:03 +0600

    Initial Commit of Video-Hooks

---
+++ videos-hooks/src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = "AIzaSyBTFC3QtDWipyM4OU96UDcW3AlCGnrVcm0";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    naxResult: 10,
+    key: KEY,
+  },
+});
