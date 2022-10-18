1e3c3456e0517b1810cbda38ca2d4c304338f55f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-18 19:02:23 +0600

    Basic Component Routing

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -34,6 +34,30 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items}></Accordion>;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search></Search>;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown></Dropdown>;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate></Translate>;
+  }
+};
+
 const App = () => {
   // const [selected, setSelected] = useState(options[0]);
   // const [showDropdown, setShowDropdown] = useState(true);
@@ -52,7 +76,11 @@ const App = () => {
           options={options}
         ></Dropdown>
       ) : null} */}
-      <Translate></Translate>
+      {/* <Translate></Translate> */}
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
