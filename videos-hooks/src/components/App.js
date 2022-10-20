4c4ec6fb7495d40c98612f6f1959ffd3e09b4896
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-20 19:31:30 +0600

    Extracting Video Logic

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -5,23 +5,12 @@ import VideoDetail from "./VideoDetail";
 import VideoList from "./VideoList";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(() => {
-    onTermSubmit("Buildings");
-  }, []);
 
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
+  // setSelectedVideo(response.data.items[0]);
 
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+
 
   return (
     <div className="ui container">
